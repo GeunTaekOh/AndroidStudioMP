ad7f0aead556f37c796924e23d81406c2fae74a7
oh geun taek <rmsxor94@naver.com>
2016-11-14 12:40:54 +0900

    add todo intent

--- MyLocationLogger2/app/src/main/java/com/taek_aaa/mylocationlogger/MapsActivity.java
+++ MyLocationLogger2/app/src/main/java/com/taek_aaa/mylocationlogger/MapsActivity.java
@@ -1,12 +1,16 @@
 package com.taek_aaa.mylocationlogger;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -15,15 +19,14 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.Date;
+
 import static com.google.android.gms.maps.CameraUpdateFactory.newLatLng;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
-    private PolylineOptions polylineOptions;
-
-
-
+    Events events;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+/*                mMap.setOnMarkerClickListener(new OnMarkerClickListener(){
+
+
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(),"컨텍스트클릭",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        );*/
     }
 
     @Override
@@ -53,27 +67,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(newLatLng(mact.alistlocation.get(0)));
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                Toast.makeText(getApplicationContext(), marker.getTitle() + "클릭했음", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), marker.getTitle() + "일정을 추가합니다.", Toast.LENGTH_SHORT).show();
+                Date clsTime = new Date();
+
+
+                Intent clsIntent = new Intent( Intent.ACTION_INSERT );
+                clsIntent.setData( events.CONTENT_URI );
+                clsIntent.putExtra( CalendarContract.EXTRA_EVENT_BEGIN_TIME, clsTime.getTime( ) );
+                clsIntent.putExtra( CalendarContract.EXTRA_EVENT_END_TIME, clsTime.getTime() + 3600000 );
+                clsIntent.putExtra( events.TITLE, "일정 제목" );
+                clsIntent.putExtra( events.DESCRIPTION, "일정 내용" );
+
+                startActivity( clsIntent );
+
                 return false;
             }
         });
 
-  /*      mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
-
-
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-
-
-            }
-        });*/
-
 
     }
-
-
-
 }
